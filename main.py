183800371c10b69aa7466eda58e70b058206ff83
angelmh2012-cloud <angelmh2012@gmail.com>
2026-06-28 13:43:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-print("Starting")
-
 import board
-
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
-from kmk.scanners import DiodeOrientation
 
 keyboard = KMKKeyboard()
 
-keyboard.col_pins = (board.GP0,)
-keyboard.row_pins = (board.GP1,)
-keyboard.diode_orientation = DiodeOrientation.COL2ROW
+# Configuración genérica de pines (Ajústalos a los pines reales de tu placa)
+# En este ejemplo usamos una matriz de 2x2 para las 4 teclas
+keyboard.col_pins = (board.GP0, board.GP1)
+keyboard.row_pins = (board.GP2, board.GP3)
+keyboard.diode_orientation = col2row
 
+# --- DEFINICIÓN DE ATRAJOS DE BLENDER ---
+VISTA_Z      = KC.Z         # Menú de sombreado (Wireframe/Solid)
+ENFOCAR_OBJ  = KC.PDOT      # Hace zoom/focus al objeto seleccionado
+OCULTAR_OBJ  = KC.H         # Oculta el objeto seleccionado
+MOSTRAR_TODO = KC.LALT(KC.H) # Revela todo lo oculto (Alt + H)
+
+# --- MAPA DE LAS 4 TECLAS ---
+# El orden aquí abajo define qué botón físico hace qué cosa:
 keyboard.keymap = [
-    [KC.A,]
+    [
+        # Fila 1: Botón Izquierdo, Botón Derecho
+        VISTA_Z,           ENFOCAR_OBJ,
+        
+        # Fila 2: Botón Izquierdo, Botón Derecho
+        OCULTAR_OBJ,       MOSTRAR_TODO
+    ]
 ]
 
 if __name__ == '__main__':
