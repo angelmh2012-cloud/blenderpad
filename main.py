72f952669b2a98f301522c3b17dd9c051ec9e9a9
angelmh2012-cloud <angelmh2012@gmail.com>
2026-06-30 19:52:41 -0600

    Refactor keyboard setup and add new key action

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ keyboard = KMKKeyboard()
 
 # Configuración genérica de pines (Ajústalos a los pines reales de tu placa)
 # En este ejemplo usamos una matriz de 2x2 para las 4 teclas
-keyboard.col_pins = (board.GP0, board.GP1)
-keyboard.row_pins = (board.GP2, board.GP3)
+keyboard.col_pins = (board.GP0)
+keyboard.row_pins = (board.GP1, board.GP2, board.GP3)
 keyboard.diode_orientation = col2row
 
 # --- DEFINICIÓN DE ATRAJOS DE BLENDER ---
 VISTA_Z      = KC.Z         # Menú de sombreado (Wireframe/Solid)
 ENFOCAR_OBJ  = KC.PDOT      # Hace zoom/focus al objeto seleccionado
-OCULTAR_OBJ  = KC.H         # Oculta el objeto seleccionado
+DUPLICAR_OBJ  = KC.LSFT(KC.D)         # Oculta el objeto seleccionado
 MOSTRAR_TODO = KC.LALT(KC.H) # Revela todo lo oculto (Alt + H)
 
 # --- MAPA DE LAS 4 TECLAS ---
@@ -24,7 +24,7 @@ keyboard.keymap = [
         VISTA_Z,           ENFOCAR_OBJ,
         
         # Fila 2: Botón Izquierdo, Botón Derecho
-        OCULTAR_OBJ,       MOSTRAR_TODO
+        DUPLICAR_OBJ,       MOSTRAR_TODO
     ]
 ]
 
